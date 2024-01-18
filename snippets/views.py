272a1ae50fd580fe847a695da89333562952e12a
Juma Antony <jumaantony840@gmail.com>
2024-01-18 12:42:44 +0300

    using mixins

--- snippets/views.py
+++ snippets/views.py
@@ -73,54 +73,87 @@ def snippet_detail(request, pk, format=None):
 """
 
 # Class Based Views
-class SnippetList(APIView):
-    """
-    List all snippets, or create a new snippet.
-    """
+# class SnippetList(APIView):
+#     """
+#     List all snippets, or create a new snippet.
+#     """
+#     @swagger_auto_schema(responses={200: SnippetSerializer(many=True)}, operation_summary='Get All Snippets')
+#     def get(self, request, format=None):
+#         snippets = Snippet.objects.all()
+#         serializer= SnippetSerializer(snippets, many=True)
+#         return JsonResponse(serializer.data, safe=False)
+
+#     @swagger_auto_schema(request_body=SnippetSerializer, responses={201: SnippetSerializer()}, operation_summary='Create a Snippet')
+#     def post(self, request, format=None):
+#         serializer = SnippetSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+#         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST, safe=False)
+
+
+# class SnippetDetail(APIView):
+#     """
+#     Retieve, update or delete a snippet instance.
+#     """
+#     def get_object(self, pk):
+#         try:
+#             return Snippet.objects.get(pk=pk)
+#         except Snippet.DoesNotExist:
+#             raise Http404
+
+#     @swagger_auto_schema(responses={200: SnippetSerializer()}, operation_summary='Get Snippet by ID')
+#     def get(self, request, pk, format=None):
+#         snippet = self.get_object(pk)
+#         serializer = SnippetSerializer(snippet)
+#         return JsonResponse(serializer.data, safe=False)
+
+#     @swagger_auto_schema(request_body=SnippetSerializer, responses={200: SnippetSerializer()}, operation_summary='Update Snippet')
+#     def patch(self, request, pk, format=None):
+#         snippet = self.get_object(pk)
+#         serializer = SnippetSerializer(snippet, data=request.data, partial=True)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data, safe=False)
+#         return JsonResponse(serializer.errors, status=status.HTTP_404_NOT_FOUND, safe=False)
+
+#     @swagger_auto_schema(responses={204: ''}, operation_summary='Delete Snippet')
+#     def delete(self, request, pk, format=None):
+#         snippet = self.get_object(pk)
+#         snippet.delete()
+#         return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+
+
+# Using mixins
+from rest_framework import mixins
+from rest_framework import generics
+
+
+class SnippetList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
     @swagger_auto_schema(responses={200: SnippetSerializer(many=True)}, operation_summary='Get All Snippets')
-    def get(self, request, format=None):
-        snippets = Snippet.objects.all()
-        serializer= SnippetSerializer(snippets, many=True)
-        return JsonResponse(serializer.data, safe=False)
-    
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
     @swagger_auto_schema(request_body=SnippetSerializer, responses={201: SnippetSerializer()}, operation_summary='Create a Snippet')
-    def post(self, request, format=None):
-        serializer = SnippetSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST, safe=False)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-class SnippetDetail(APIView):
-    """
-    Retieve, update or delete a snippet instance.
-    """
-    def get_object(self, pk):
-        try:
-            return Snippet.objects.get(pk=pk)
-        except Snippet.DoesNotExist:
-            raise Http404
-    
-    @swagger_auto_schema(responses={200: SnippetSerializer()}, operation_summary='Get Snippet by ID')    
-    def get(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet)
-        return JsonResponse(serializer.data, safe=False)
-    
+class SnippetDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+    @swagger_auto_schema(responses={200: SnippetSerializer()}, operation_summary='Get Snippet by ID')
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
     @swagger_auto_schema(request_body=SnippetSerializer, responses={200: SnippetSerializer()}, operation_summary='Update Snippet')
-    def patch(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, safe=False)
-        return JsonResponse(serializer.errors, status=status.HTTP_404_NOT_FOUND, safe=False)
-    
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
     @swagger_auto_schema(responses={204: ''}, operation_summary='Delete Snippet')
-    def delete(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        snippet.delete()
-        return HttpResponse(status=status.HTTP_204_NO_CONTENT)   
-    
-    
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
